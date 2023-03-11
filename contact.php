7f242e0051facd1d266a81b4182db5e2c1861227
prajyot <prajyotpatil17004@gmail.com>
2023-03-11 22:21:00 +0530

    new

--- contact.php
+++ contact.php
@@ -94,6 +94,33 @@
 
 </div>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $name = $_POST['c_name'];
+  $phone = $_POST['phone'];
+  $gmail = $_POST['gmail'];
+  $Address = $_POST['Address'];
+  $comment = $_POST['comment'];
+
+  $sql = "INSERT INTO `client_data`(`client_name`, `client_number`, `gmail`, `c_address`, `comment`) VALUES ('$name','$phone','$gmail','$Address','$comment')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully";
+} 
+catch(PDOException $e) {
+  
+}
+
+$conn = null;
+?>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
