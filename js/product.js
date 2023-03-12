4bfcd2f9d35dadd527aa229631f97846410f2326
prajyot <prajyotpatil17004@gmail.com>
2023-03-12 11:24:55 +0530

    dg

--- js/product.js
+++ js/product.js
@@ -1,17 +1,17 @@
-
+let a=0;
 function myFunction() {
  
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
-  if (more.style.display === "none") {
+  if (a==0) {
     moreText.style.display = "inline";
     btnText.innerHTML = "Read less"; 
-    
+    a=1;
   } else {
     more.style.display = "none";
     btnText.innerHTML = "Read more"; 
-    
+    a=0;
   }
 }
 function myFunction2() {
@@ -19,14 +19,14 @@ function myFunction2() {
     var moreText = document.getElementById("more2");
     var btnText = document.getElementById("myBtn2");
   
-    if (more2.style.display === "none") {
+    if (a==0) {
       moreText.style.display = "inline";
       btnText.innerHTML = "Read less"; 
-      
+      a=1;
     } else {
       more2.style.display = "none";
       btnText.innerHTML = "Read more"; 
-      
+      a=0;
     }
   }
   function myFunction3() {
@@ -34,13 +34,13 @@ function myFunction2() {
     var moreText = document.getElementById("more3");
     var btnText = document.getElementById("myBtn3");
   
-    if (more3.style.display === "none") {
+    if (a==0) {
       moreText.style.display = "inline";
       btnText.innerHTML = "Read less"; 
-      
+      a=1;
     } else {
       more3.style.display = "none";
       btnText.innerHTML = "Read more"; 
-      
+      a=0;
     }
   }
